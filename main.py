49908dab2556ab8e67bb8f24c5f08c79dc889725
Sunjana Ramana <sunjana2199@gmail.com>
2024-09-05 18:23:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/name")
+async def name(self, value):
+    return {"message": f"Hi I am {value}"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
